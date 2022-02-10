6f924d77ba91864f037d7471003f6617d681ff20
Thays Omine <thaysomine@gmail.com>
2022-02-10 02:57:57 -0300

    funçoes para selecionar com quem e como falar adicionadas

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let contact = null;
+let mode = null;
 // função para abrir barra lateral
 function showSideBar() {
     document.querySelector("aside").classList.remove("hidden")
@@ -10,7 +12,6 @@ function unShowSideBar() {
         document.querySelector("main").classList.remove("bright");
     }
 }
-
 // função para adicionar mensagens
 function sendMessage () {
     const input = document.querySelector("footer input").value;
@@ -26,5 +27,23 @@ function sendMessage () {
         `;
     input.value = "";
     console.log(input);
+}
+// função para escolher destinatario da mensagem 
+function receiver (receiverOption) {
+    if(contact !== null) {
+        contact.querySelector("aside .check").classList.add("hidden");
+    }
+
+    contact = receiverOption;
+    contact.querySelector("aside .check").classList.remove("hidden");
+}
+// função para escolher entre mensagem publica ou privada
+function typeMessage(typeOption) {
+    if(mode !== null) {
+        mode.querySelector("aside .check").classList.add("hidden");
+    }
+
+    mode = typeOption;
+    mode.querySelector("aside .check").classList.remove("hidden");
 }
 
